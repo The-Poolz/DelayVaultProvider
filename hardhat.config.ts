1e2d279b3e7ee1fc97bb4be92b350551949135eb
idoh-spherex <ido.h@sphere-x.xyz>
2023-11-01 19:12:51 +0200

    Fixed optimizer settings.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,21 +17,14 @@ function getRemappings() {
 const config: HardhatUserConfig = {
     defaultNetwork: "hardhat",
     solidity: {
-        compilers: [
-            {
-                version: "0.8.0",
-            },
-            {
-                version: "0.8.19",
-            },
-        ],
-        settings: {
-            evmVersion: "istanbul",
-            optimizer: {
-                enabled: true,
-                runs: 200,
-            },
+      version: '0.8.19',
+      settings: {
+        evmVersion: 'istanbul',
+        optimizer: {
+          enabled: true,
+          runs: 200,
         },
+      },
     },
     networks: {
         hardhat: {
