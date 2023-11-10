08f0b0d26bfb5d078e7bc5cb5eeaef780a140a38
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-11-10 10:50:25 +0200

    added tests

--- test/DelayVaultProvider/DelayVaultView.ts
+++ test/DelayVaultProvider/DelayVaultView.ts
@@ -1,7 +1,10 @@
 import { delayVault } from './setupTests';
 import { expect } from 'chai';
+import { ethers } from 'hardhat';
 
 describe('DelayVaultProvider view tests', async () => {
+  const params = [delayVault.tier2];
+
   before(async () => {
     await delayVault.initialize();
   });
@@ -11,7 +14,6 @@ describe('DelayVaultProvider view tests', async () => {
   });
 
   it('should return getWithdrawableAmount from created pool', async () => {
-    const params = [delayVault.tier2];
     const owner = delayVault.newOwner;
     await delayVault.token.connect(owner).approve(delayVault.delayVaultProvider.address, delayVault.tier2);
     await delayVault.delayVaultProvider.connect(owner).createNewDelayVault(owner.address, params);
@@ -25,11 +27,54 @@ describe('DelayVaultProvider view tests', async () => {
   });
 
   it('should return total user amount', async () => {
-    const params = [delayVault.tier2];
     const owner = delayVault.user1;
     await delayVault.token.connect(owner).approve(delayVault.delayVaultProvider.address, delayVault.tier3);
     await delayVault.delayVaultProvider.connect(owner).createNewDelayVault(owner.address, params);
     await delayVault.delayVaultProvider.connect(owner).createNewDelayVault(owner.address, params);
     expect(await delayVault.delayVaultProvider.getTotalAmount(owner.address)).to.be.equal(delayVault.tier2.mul(2));
+  });
+
+  it('should return user nft token balance', async () => {
+    const count = 10;
+    const owner = delayVault.user2;
+    await delayVault.token.connect(owner).approve(delayVault.delayVaultProvider.address, delayVault.tier2.mul(count));
+    // create 10 NFTs
+    for (let i = 0; i < count; ++i) {
+      await delayVault.delayVaultProvider.connect(owner).createNewDelayVault(owner.address, params);
+    }
+    expect(await delayVault.delayVaultProvider.balanceOf(owner.address)).to.be.equal(count);
+  });
+
+  it("should only return DelayVault NFT's balanceOf", async () => {
+    // create dealProvider
+    const owner = delayVault.user3;
+    const currentNonce = await delayVault.vaultManager.nonces(owner.address);
+    const dataToCheck = ethers.utils.solidityPack(['address', 'uint256'], [delayVault.token.address, delayVault.tier1]);
+    const hash = ethers.utils.solidityKeccak256(['bytes', 'uint'], [dataToCheck, currentNonce]);
+    const signature = await owner.signMessage(ethers.utils.arrayify(hash));
+    await delayVault.token.connect(owner).approve(delayVault.vaultManager.address, delayVault.tier2);
+    await delayVault.dealProvider.connect(owner).createNewPool([owner.address, delayVault.token.address], [delayVault.tier1], signature);
+    // create 2 delayVault NFTs
+    await delayVault.token.connect(owner).approve(delayVault.delayVaultProvider.address, delayVault.tier2.mul(2));
+    await delayVault.delayVaultProvider.connect(owner).createNewDelayVault(owner.address, params);
+    await delayVault.delayVaultProvider.connect(owner).createNewDelayVault(owner.address, params);
+    expect(await delayVault.delayVaultProvider.balanceOf(owner.address)).to.be.equal(2);
+  });
+
+  it('should return users nft poolIds by indexes', async () => {
+    const poolId = (await delayVault.lockDealNFT.totalSupply()).toNumber();
+    const owner = delayVault.user4;
+    await delayVault.token.connect(owner).approve(delayVault.delayVaultProvider.address, delayVault.tier2.mul(5));
+    // create 5 NFTs and check their poolIds by indexes
+    for (let i = 0; i < 5; ++i) {
+      await delayVault.delayVaultProvider.connect(owner).createNewDelayVault(owner.address, params);
+      expect(await delayVault.delayVaultProvider.tokenOfOwnerByIndex(owner.address, i)).to.be.equal(poolId + i);
+    }
+  });
+
+  it('should revert invalid index in the tokenOfOwnerByIndex call', async () => {
+    await expect(delayVault.delayVaultProvider.tokenOfOwnerByIndex(delayVault.receiver.address, 0)).to.be.revertedWith(
+      'invalid index poolId',
+    );
   });
 });
