018724f5fd12bed7bb46e28d15ec3546aa2251bc
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-11-21 13:42:08 +0200

    update NatSpec comments

--- contracts/HoldersSum.sol
+++ contracts/HoldersSum.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "./LockDealNFT/contracts/SimpleProviders/Provider/ProviderModifiers.sol";
 import "./interfaces/IDelayVaultProvider.sol";
-import "./interfaces/IDelayVaultV1.sol";
 import "./interfaces/IMigrator.sol";
 
 /**
